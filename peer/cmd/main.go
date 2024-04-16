9b8e1e3de9596606afc97a8fc28fed975c39bb24
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-15 23:51:34 -0400

    Remove: old code for creating a test server

--- peer/cmd/main.go
+++ peer/cmd/main.go
@@ -4,23 +4,15 @@ import (
 	"flag"
 	orcaCLI "orca-peer/internal/cli"
 	orcaHash "orca-peer/internal/hash"
-	orcaTest "orca-peer/test"
 	"os"
 )
 
-var test bool
-var localOnly bool
 var boostrapNodeAddress string
 
 func main() {
-	flag.BoolVar(&test, "test", false, "Create test server with no CLI.")
 	flag.StringVar(&boostrapNodeAddress, "bootstrap", "", "Give address to boostrap.")
 	flag.Parse()
 	publicKey, privateKey := orcaHash.LoadInKeys()
 	os.MkdirAll("./files/stored/", 0755)
-	if test {
-		orcaTest.RunTestServer()
-	} else {
-		orcaCLI.StartCLI(&boostrapNodeAddress, publicKey, privateKey)
-	}
+	orcaCLI.StartCLI(&boostrapNodeAddress, publicKey, privateKey)
 }
