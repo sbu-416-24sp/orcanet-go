084399555333c8452349913161415bd0defef2d9
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-04-21 00:56:19 -0400

    Add backend functionality for changing job status, not connected to actual request

--- peer/internal/jobs/api.go
+++ peer/internal/jobs/api.go
@@ -1,5 +1,15 @@
 package jobs
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Job struct {
 	FileHash        string `json:"fileHash"`
 	JobId           string `json:"jobID"`
@@ -10,15 +20,82 @@ type Job struct {
 	ETA             int    `json:"eta"`
 	PeerId          string `json:"peer"`
 }
-type JobInfo struct {
+
+type JobManager struct {
+	Jobs    []Job
+	Mutex   sync.Mutex
+	Changed bool
 }
 
-func RemoveFromHistoryHandler() {
+var manager JobManager
 
+func InitPeriodicJobSave() {
+	manager = JobManager{
+		Jobs:    make([]Job, 0), // Initialize an empty slice of jobs
+		Mutex:   sync.Mutex{},   // Initialize a mutex
+		Changed: false,
+	}
+	for {
+		time.Sleep(10 * time.Second)
+		manager.Mutex.Lock()
+		if manager.Changed {
+			SaveHistory(manager.Jobs)
+		}
+		manager.Mutex.Unlock()
+	}
+}
+func writeStatusUpdate(w http.ResponseWriter, message string) {
+	responseMsg := map[string]interface{}{
+		"status": message,
+	}
+	responseMsgJsonString, err := json.Marshal(responseMsg)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseMsgJsonString)
 }
 
-func ClearHistoryHandler() {
+type RmFromHistoryReqPayload struct {
+	JobId string `json:"jobID"`
+}
 
+func RemoveFromHistoryHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var payload RmFromHistoryReqPayload
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&payload); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			writeStatusUpdate(w, "Cannot marshal payload in Go object. Does the payload have the correct body structure?")
+			return
+		}
+		err := RemoveFromHistory(payload.JobId)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			writeStatusUpdate(w, err.Error())
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		writeStatusUpdate(w, "success")
+		return
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeStatusUpdate(w, "Only PATCH requests will be handled.")
+		return
+	}
+}
+
+func ClearHistoryHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		ClearHistory()
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeStatusUpdate(w, "Only PATCH requests will be handled.")
+		return
+	}
 }
 
 type AddJobReqPayload struct {
@@ -30,8 +107,35 @@ type AddJobResPayload struct {
 	JobId string `json:"jobID"`
 }
 
-func AddJobHandler() {
-
+func AddJobHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPut {
+		var payload AddJobReqPayload
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&payload); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			writeStatusUpdate(w, "Cannot marshal payload in Go object. Does the payload have the correct body structure?")
+			return
+		}
+		id := uuid.New()
+		timeString := time.Now().Format(time.RFC3339)
+		newJob := Job{
+			FileHash:        payload.FileHash,
+			JobId:           id.String(),
+			TimeQueued:      timeString,
+			Status:          "paused",
+			AccumulatedCost: 0,
+			ProjectedCost:   -1,
+			ETA:             -1,
+			PeerId:          payload.PeerId,
+		}
+		AddJob(newJob)
+		w.WriteHeader(http.StatusOK)
+		writeStatusUpdate(w, "Successfully added job.")
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeStatusUpdate(w, "Only PUT requests will be handled.")
+		return
+	}
 }
 
 type JobInfoReqPayload struct {
@@ -59,14 +163,75 @@ type JobPeerResPayload struct {
 func JobPeerHandler() {
 
 }
-func StartJobsHandler() {
-
+func StartJobsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var jobIds []JobInfoReqPayload
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&jobIds); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			writeStatusUpdate(w, "Cannot marshal payload in Go object. Does the payload have the correct body structure?")
+			return
+		}
+		for _, jobId := range jobIds {
+			err := StartJob(jobId.JobId)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				writeStatusUpdate(w, err.Error())
+			}
+		}
+		w.WriteHeader(http.StatusOK)
+		writeStatusUpdate(w, "Successfully added job.")
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeStatusUpdate(w, "Only PATCH requests will be handled.")
+	}
 }
 
-func PauseJobsHandler() {
-
+func PauseJobsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var jobIds []JobInfoReqPayload
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&jobIds); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			writeStatusUpdate(w, "Cannot marshal payload in Go object. Does the payload have the correct body structure?")
+			return
+		}
+		for _, jobId := range jobIds {
+			err := PauseJob(jobId.JobId)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				writeStatusUpdate(w, err.Error())
+			}
+		}
+		w.WriteHeader(http.StatusOK)
+		writeStatusUpdate(w, "Successfully added job.")
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeStatusUpdate(w, "Only PATCH requests will be handled.")
+	}
 }
 
-func TerminateJobsHandler() {
-
+func TerminateJobsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPatch {
+		var jobIds []JobInfoReqPayload
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&jobIds); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			writeStatusUpdate(w, "Cannot marshal payload in Go object. Does the payload have the correct body structure?")
+			return
+		}
+		for _, jobId := range jobIds {
+			err := TerminateJob(jobId.JobId)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				writeStatusUpdate(w, err.Error())
+			}
+		}
+		w.WriteHeader(http.StatusOK)
+		writeStatusUpdate(w, "Successfully added job.")
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		writeStatusUpdate(w, "Only PATCH requests will be handled.")
+		return
+	}
 }
--- peer/internal/jobs/job.go
+++ peer/internal/jobs/job.go
@@ -2,8 +2,162 @@ package jobs
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 )
+
+/*
+
+### Endpoints
+/add-job
+  Description: Adds a job with an associated peer.
+  PUT
+  Parameters:
+    fileHash: string
+    peerID: PeerID
+  Returns:
+    {
+      jobID: JobID
+    }
+    Success or error
+
+/find-peer
+  Description: Finds all peers there at are hosting a given file.
+  GET
+  Parameters:
+    fileHash: string
+  Returns:
+    {
+      peers: {
+        peerID: PeerID
+        ip: string
+        region: string
+        price: float
+        reputation: int
+      }[]
+    }
+
+/add-peer
+  Description: Adds a peer to a job.
+  PUT
+  Parameters:
+    jobID: JobID
+    peerID: PeerID
+  Returns:
+    Success or error
+/job-list
+  Description: Returns list of current jobs.
+  GET
+  Parameters:
+    none
+  Returns:
+    {
+      jobs: {
+        jobID: JobID,
+        fileName: string,
+        fileSize: int // bytes,
+        eta: int // seconds
+        timeQueued: Date,
+        status: "active" | "paused" | "error" | "completed",
+      }[]
+    }
+
+/job-info
+  Description: Gets the job information for a specific job
+  GET
+  Parameters:
+    jobID: JobID
+  Returns:
+    {
+      fileHash: string,
+      fileName: string,
+      fileSize: float,
+      accumulatedMemory: float,
+      accumulatedCost: float,
+      projectedCost: float,
+      eta: int,
+      peer: PeerID,
+    }
+/job-peer
+  Description: Gets the peer information for a specific job
+  GET
+  Parameters:
+    jobID: JobID
+    peerID: PeerID
+  Returns:
+    {
+      ipAddress: string,
+      region: string,
+      liked: boolean,
+      status: "uninitialized" | "active" | "paused" | "error" | "terminated",
+      accumulatedMemory: float,
+      price: int,
+      graph: { time: float, speed: float }
+    }
+/start-jobs
+  Description: Begin using peer to download in job(s)
+  PATCH
+  Parameters:
+    jobIDs: JobID[]
+  Return:
+    Success or error
+
+/pause-jobs
+  Description: Stop using peer to download in job(s)
+  PATCH
+  Parameters:
+    jobIDs: JobID[]
+  Return:
+    Success or error
+
+/terminate-jobs
+  Description: Cancel job(s)
+  PATCH
+  Parameters:
+    jobIDs: JobID[]
+  Return:
+    Success or error
+
+/favorite-peer
+  Description: Add peer to user’s favorite list
+  PATCH
+  Parameters:
+    peerID: PeerID
+  Return:
+    Success or error
+
+/unfavorite-peer
+  Description: Delete peer in user’s favorite list
+  PATCH
+  Parameters:
+    peerID: PeerID
+  Return:
+    Success or error
+
+/like-peer
+  Description: Adds a point to peer’s reputation
+  PATCH
+  Parameters:
+    peerID: PeerID
+  Return:
+    Success or error
+
+/unlike-peer
+  Description: Removes a point in peer’s reputation
+  PATCH
+  Parameters:
+    peerID: PeerID
+  Return:
+    Success or error
+
+*/
+
+func AddJob(job Job) {
+	manager.Mutex.Lock()
+	manager.Jobs = append(manager.Jobs, job)
+	manager.Changed = true
+	manager.Mutex.Unlock()
+}
 
 func LoadHistory() ([]Job, error) {
 	fileData, err := os.ReadFile("./internal/jobs/jobs.json")
@@ -18,6 +172,7 @@ func LoadHistory() ([]Job, error) {
 	return jobs, nil
 }
 func SaveHistory(jobs []Job) error {
+	manager.Changed = false
 	jsonData, err := json.Marshal(jobs)
 	if err != nil {
 		return err
@@ -27,4 +182,73 @@ func SaveHistory(jobs []Job) error {
 		return err
 	}
 	return nil
+}
+
+func RemoveFromHistory(jobId string) error {
+	manager.Mutex.Lock()
+	for idx, job := range manager.Jobs {
+		if job.JobId == jobId {
+			manager.Jobs = append(manager.Jobs[:idx], manager.Jobs[idx+1:]...)
+			manager.Changed = true
+			manager.Mutex.Unlock()
+			return nil
+		}
+	}
+	manager.Mutex.Unlock()
+	return errors.New("unable to find job that matches jobID")
+}
+
+func ClearHistory() {
+	manager.Mutex.Lock()
+	newJobs := make([]Job, 0)
+	for _, job := range manager.Jobs {
+		if job.Status != "completed" {
+			newJobs = append(newJobs, job)
+		}
+	}
+	manager.Jobs = newJobs
+	manager.Changed = true
+	manager.Mutex.Unlock()
+}
+
+func TerminateJob(jobId string) error {
+	manager.Mutex.Lock()
+	for idx, job := range manager.Jobs {
+		if job.JobId == jobId {
+			manager.Jobs[idx].Status = "terminated"
+			manager.Changed = true
+			manager.Mutex.Unlock()
+			return nil
+		}
+	}
+	manager.Mutex.Unlock()
+	return errors.New("Unable to find jobId: " + jobId)
+}
+
+func PauseJob(jobId string) error {
+	manager.Mutex.Lock()
+	for idx, job := range manager.Jobs {
+		if job.JobId == jobId {
+			manager.Jobs[idx].Status = "paused"
+			manager.Changed = true
+			manager.Mutex.Unlock()
+			return nil
+		}
+	}
+	manager.Mutex.Unlock()
+	return errors.New("Unable to find jobId: " + jobId)
+}
+
+func StartJob(jobId string) error {
+	manager.Mutex.Lock()
+	for idx, job := range manager.Jobs {
+		if job.JobId == jobId {
+			manager.Jobs[idx].Status = "active"
+			manager.Changed = true
+			manager.Mutex.Unlock()
+			return nil
+		}
+	}
+	manager.Mutex.Unlock()
+	return errors.New("Unable to find jobId: " + jobId)
 }
