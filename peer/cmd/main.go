a7d1a706cb7c93d65b8fd907b869753fa2c8fc5d
Surya Patil <patil.surya01@gmail.com>
2024-04-22 18:47:57 -0400

    merged orcacoin

--- peer/cmd/main.go
+++ peer/cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	orcaCLI "orca-peer/internal/cli"
 	orcaHash "orca-peer/internal/hash"
 	orcaTest "orca-peer/test"
 	"os"
+	"os/exec"
 )
 
 var test bool
@@ -18,6 +20,15 @@ func main() {
 	flag.Parse()
 	publicKey, privateKey := orcaHash.LoadInKeys()
 	os.MkdirAll("./files/stored/", 0755)
+
+	fmt.Println("**Starting Blockchain Server**")
+	const executablePath string = "/Users/suryapatil/OrcaNetAPIServer/OrcaNetAPIServer"
+	cmd := exec.Command(executablePath)
+	cmd.Start()
+	fmt.Println("**Starting Blockchain Server started**")
+
+
+
 	if test {
 		orcaTest.RunTestServer()
 	} else {
