cc46b617eefa5bf2b72b5645f0113c38c52a38c2
Surya Patil <patil.surya01@gmail.com>
2024-04-29 18:26:25 -0400

    merging ReadMEs

--- peer/cmd/main.go
+++ peer/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	orcaAPI "orca-peer/internal/api"
 	orcaCLI "orca-peer/internal/cli"
 	orcaHash "orca-peer/internal/hash"
 	"os"
@@ -16,4 +17,14 @@ func main() {
 	flag.Parse()
 	publicKey, privateKey := orcaHash.LoadInKeys()
 	os.MkdirAll("./files/stored/", 0755)
+
+	cmd := exec.Command("./OrcaNetAPIServer")
+	cmd.Dir = "../coin/"
+	err := cmd.Start()
+	if err != nil {
+		fmt.Printf("Error starting OrcaNetAPIServer: %s\n", err)
+		return
+	}
+	fmt.Println("Started block chain api server")
+	orcaCLI.StartCLI(&boostrapNodeAddress, publicKey, privateKey, cmd, orcaAPI.InitServer)
 }
